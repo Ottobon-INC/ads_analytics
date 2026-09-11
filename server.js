275a73ddcf25c5d58019434552837a362f1daed1
Sudheer <sudheernistala@gmail.com>
2026-09-11 12:32:59 +0530

    fix: express 5 wildcard routing error

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(distPath));
   
   // SPA fallback for routing
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
